6780336bf7f398073766c8cc2770ec686938dcb7
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-29 11:08:23 +0600

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,11 +16,21 @@ class Course < ApplicationRecord
   validates :language, presence: true
   validates :duration, presence: true
 
-  def duration_hours
-    duration ? duration / 1.hour : 0
-  end
+  # def duration_hours
+  #   duration ? duration / 1.hour : 0
+  # end
 
-  def duration_minutes
-    duration ? (duration % 1.hour) / 1.minute : 0
+  # def duration_minutes
+  #   duration ? (duration % 1.hour) / 1.minute : 0
+  # end
+
+  def total_duration
+    return "0 hours, 0 minutes" unless duration
+
+    total_minutes = (duration / 1.minute).round
+    hours = total_minutes / 60
+    minutes = total_minutes % 60
+
+    "#{hours} hour#{'s' if hours != 1}, #{minutes} minute#{'s' if minutes != 1}"
   end
 end
