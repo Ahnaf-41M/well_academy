3e7484a1b043e7c5f4bcb6ddf95bd2f631191286
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-29 15:33:40 +0600

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,6 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    # @confirmation_url = confirmation_url(token: @user.confirmation_token)
-    # @confirmation_url = url_for(controller: 'users', action: 'confirm', token: @user.confirmation_token, only_path: false)
     @confirmation_url = confirmation_user_url(@user, token: @user.confirmation_token)
 
     mail(to: @user.email, subject: 'Welcome to Our App!')
