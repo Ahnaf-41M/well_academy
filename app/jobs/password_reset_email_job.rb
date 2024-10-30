936bed1e3615cb3764f8a29882a82ba984ccb14e
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-30 13:39:55 +0600

    Update password_reset_email_job.rb

--- app/jobs/password_reset_email_job.rb
+++ app/jobs/password_reset_email_job.rb
@@ -4,6 +4,7 @@ class PasswordResetEmailJob
 
   def perform(user_id)
     user = User.find(user_id)
+    user.generate_password_reset_token!
     UserMailer.password_reset(user).deliver_now if user.reset_password_token.present?
   end
 end
