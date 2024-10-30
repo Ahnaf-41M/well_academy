1c74977c701f3436030157e77a2ec12c64caabed
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-30 11:12:43 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,15 @@ class User < ApplicationRecord
   def generate_confirmation_token
     self.confirmation_token = SecureRandom.hex(10)
   end
+
+  def generate_password_reset_token!
+    update!(
+      reset_password_token: SecureRandom.urlsafe_base64,
+      reset_password_sent_at: Time.current
+    )
+  end
+
+  def password_reset_token_expired?
+    reset_password_sent_at < 2.hours.ago
+  end
 end
