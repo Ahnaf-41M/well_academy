406b36e8959f71ac660cf33e11e973ab1da24f80
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-30 11:12:40 +0600

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,12 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @confirmation_url = confirmation_user_url(@user, token: @user.confirmation_token)
+    mail(to: @user.email, subject: 'Welcome to WellAcademy!')
+  end
 
-    mail(to: @user.email, subject: 'Welcome to Our App!')
+  def password_reset(user)
+    @user = user
+    @reset_link = edit_password_reset_url(@user.reset_password_token)
+    mail(to: @user.email, subject: "Password Reset Instructions")
   end
 end
