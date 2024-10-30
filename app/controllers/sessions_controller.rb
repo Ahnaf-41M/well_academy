96e1082a8cc1aec10b1dcee6d9cd3c2dc73612b0
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-30 09:55:44 +0600

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   layout "application"
+
   def new
   end
 
@@ -12,9 +13,14 @@ class SessionsController < ApplicationController
   def attempt_login
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      session[:email] = user.email
-      redirect_to root_path, notice: "Logged in successfully"
+      if user.confirmed_at.nil?
+        UserMailerJob.perform_async(user.id)
+        redirect_to root_path
+      else
+        session[:user_id] = user.id
+        session[:email] = user.email
+        redirect_to root_path, notice: "Logged in successfully"
+      end
     else
       flash.now[:alert] = "Invalid email or password"
       render :login, status: :unprocessable_entity
