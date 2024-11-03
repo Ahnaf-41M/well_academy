f2af007e1c1963d5d21c9cba1ad9ab943cd7f27e
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-03 17:35:50 +0600

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, presence: true
+  has_many :courses
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
   validates :description, presence: true
 end
