b9056f4f634f5c854f92c5d8083ee55ca1997496
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-10 13:31:27 +0600

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,23 +15,22 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       if user.confirmed_at.nil? && !user.confirmation_token.nil?
         UserMailerJob.new.perform(user.id)
-        redirect_to root_path
+        redirect_to root_path, notice: t('sessions.errors.unconfirmed_account')
       else
         session[:user_id] = user.id
         session[:email] = user.email
-        redirect_to root_path, notice: "Logged in successfully"
+        redirect_to root_path, notice: t('sessions.create.success')
       end
     else
-      flash.now[:alert] = "Invalid email or password"
+      flash.now[:alert] = t('sessions.errors.invalid_credentials')
       render :login, status: :unprocessable_entity
     end
   end
 
   def destroy
-    params[:id] = nil
     session[:user_id] = nil
     session[:email] = nil
-    redirect_to root_path, notice: "Logged out successfully"
+    redirect_to root_path, notice: t('sessions.logout.success')
   end
 
   def attempt_logout
