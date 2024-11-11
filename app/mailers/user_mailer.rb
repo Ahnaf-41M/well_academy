bf4d97247206805eb9333d2a4b9c499a3e2e2021
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-11 10:33:09 +0600

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
 
   def password_reset(user)
     @user = user
-    @reset_link = edit_password_reset_url(@user.reset_password_token)
+    @reset_link = edit_password_reset_url(reset_password_token: @user.reset_password_token)
     mail(to: @user.email, subject: "Password Reset Instructions")
   end
 end
