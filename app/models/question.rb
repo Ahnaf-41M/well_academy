a7edd0e85b81d84720f0b9e9743087e9d3550243
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-14 18:19:22 +0600

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :options, dependent: :destroy
 
-  validates :content, presence: true
-  validates :options, presence: true
+  validates :content, :options, presence: true
   validates :marks, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
