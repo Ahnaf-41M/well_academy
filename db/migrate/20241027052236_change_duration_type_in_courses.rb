bbbdf290a89d9e692dda6ee4fce99e1284c41d4f
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-14 10:12:48 +0600

    Deleted migration changes

--- db/migrate/20241027052236_change_duration_type_in_courses.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDurationTypeInCourses < ActiveRecord::Migration[6.0]
-  def up
-    change_column :courses, :duration, :interval
-  end
-
-  def down
-    change_column :courses, :duration, :integer
-  end
-end
