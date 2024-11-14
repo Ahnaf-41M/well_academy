1db0fed712e9cd3c68641dc4bd57f5af583bfaca
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-14 13:44:13 +0600

    Update 20241016045036_create_enrollments.rb

--- db/migrate/20241016045036_create_enrollments.rb
+++ db/migrate/20241016045036_create_enrollments.rb
@@ -2,7 +2,7 @@ class CreateEnrollments < ActiveRecord::Migration[7.2]
   def change
     create_table :enrollments do |t|
       t.belongs_to :student, foreign_key: true, null: false
-      t.references :course, null: false, foreign_key: true
+      t.belongs_to :course, foreign_key: true, null: false
       t.timestamp :enrolled_at, default: -> { 'CURRENT_TIMESTAMP' }
       t.decimal :progress, precision: 10, scale: 2, default: 0.0
       t.integer :completion_status, null: false, default: 0  # For enum
