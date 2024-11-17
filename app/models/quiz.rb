18dff77359c710514808408ca743aa96a7cc7ef1
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-17 17:17:09 +0600

    Update quiz.rb

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,5 +3,5 @@ class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
 
   validates :title, presence: true
-  validates :total_marks, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :total_marks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
