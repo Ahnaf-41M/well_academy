77e7541c35d73a1e00ca46f09287a41eaf691ffb
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-17 17:52:22 +0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+User.destroy_all
+puts "*** Database cleared. ***"
+
+def attach_local_image(record, attachment_name, local_path)
+  file = File.open(local_path)
+  record.send(attachment_name).attach(
+    io: file,
+    filename: File.basename(local_path),
+    content_type: 'image/jpeg'
+  )
+  file.close
+end
+
 User.create!(
   name: "Kaium Uddin",
   email: "admin@a.com",
@@ -7,7 +20,23 @@ User.create!(
   role: "admin"
 )
 
-User.create!(name: "Hasib Chy", email: "hasib@example.com", password: "1234", phone: "1234567891", bio: "Demo user", role: "student")
-User.create!(name: "Samin Wasee", email: "wasee@example.com", password: "1234", phone: "1234567892", bio: "Demo user", role: "student")
-User.create!(name: "Redwan Ahmed", email: "redwan@example.com", password: "1234", phone: "1234567893", bio: "Demo user", role: "student")
-User.create!(name: "Arnab", email: "arnab@example.com", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+user = User.create!(name: "Hasib Chy", email: "hasib@welldev.io", password: "1234", phone: "1234567891", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/hasib-bhai.jpeg")
+user = User.create!(name: "Samin Al-Wasee", email: "wasee@welldev.io", password: "1234", phone: "1234567892", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/wasee-bhai.jpeg")
+user = User.create!(name: "Redwan Ahmed", email: "redwan@welldev.io", password: "1234", phone: "1234567893", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/redwan-bhai.jpeg")
+user = User.create!(name: "Arnab Saha", email: "arnab@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/arnab-bhai.jpeg")
+user = User.create!(name: "Sadman Ahmed", email: "sadman@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/sadman-bhai.jpeg")
+user = User.create!(name: "Kaium Uddin", email: "kaium@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/kaium-bhai.jpeg")
+user = User.create!(name: "Mohammad Ashikul Islam", email: "ashik@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/ashik-bhai.jpeg")
+user = User.create!(name: "Radoan Sharkar", email: "radoan@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/richi.jpeg")
+user = User.create!(name: "Tahsin Turab", email: "turab@welldev.io", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
+attach_local_image(user, :profile_picture, "/Users/ahnaf/Documents/GitHub/well_academy/app/assets/images/turab.jpeg")
+
+puts "*** Database created. ***"
