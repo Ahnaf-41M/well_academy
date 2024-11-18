47bac224184ac4717204ccab79b76441887d80ae
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-18 17:28:24 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  before_create :generate_confirmation_token
+  
   has_many :video_watches
   has_many :watched_lessons, through: :video_watches, source: :lesson
   has_many :reviews, foreign_key: :student_id, dependent: :destroy
@@ -8,7 +10,7 @@ class User < ApplicationRecord
   has_one_attached :grad_certificate
   has_one_attached :postgrad_certificate
 
-  enum role: {student: 0, teacher: 1,  admin:2}
+  enum role: { student: 0, teacher: 1, admin: 2 }
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
