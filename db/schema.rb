b74bad12076862de84da5b47162661d02ffad48b
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-18 17:28:35 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -151,6 +151,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_31_084048) do
     t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
