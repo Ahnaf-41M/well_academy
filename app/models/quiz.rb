6c45efc4bca66af864f463f1bd21e6c1b7f28a38
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-20 17:08:25 +0600

    Update quiz.rb

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,16 @@ class Quiz < ApplicationRecord
   belongs_to :course
   has_many :questions, dependent: :destroy
 
+  before_save :update_total_marks
+
   validates :title, presence: true
   validates :total_marks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  private
+
+  def update_total_marks
+    if questions
+      self.total_marks = questions.sum(:marks) # Sum the marks of all associated questions
+    end
+  end
 end
