b55c6ae03c50059ef4bbc0db87c29b8f4c46fce5
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-21 17:35:54 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   before_create :generate_confirmation_token
-  
-  has_many :video_watches
-  has_many :watched_lessons, through: :video_watches, source: :lesson
+
+  has_many :courses, foreign_key: :teacher_id, dependent: :destroy
+  has_many :video_watches, dependent: :destroy
+  has_many :watched_lessons, through: :video_watches, source: :lesson, dependent: :destroy
   has_many :reviews, foreign_key: :student_id, dependent: :destroy
+  has_many :enrollments, foreign_key: :student_id, dependent: :destroy
+  has_many :quiz_participations, foreign_key: :student_id, dependent: :destroy
+  has_many :payments, dependent: :destroy
+
   has_one_attached :profile_picture
   has_many_attached :student_certificates
   has_one_attached :grad_certificate
