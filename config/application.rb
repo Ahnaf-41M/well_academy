37681ef49c927394814151d8f83401c4bca10dfa
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-11-24 10:34:23 +0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module WellAcademy
 
     config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.exceptions_app = self.routes
+
   end
 end
