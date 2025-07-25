a9fd103a9498d6fb702697e48237a3fc0c7a8373
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2025-07-25 21:55:54 +0600

    fix: update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,40 @@
 class HomeController < ApplicationController
+  ITEMS_PER_PAGE = 5
+
   def index
     @user = current_user
 
     if params[:search].present?
       search_pattern = params[:search]
-      @courses = Course.where("title ~* ? OR description ~* ?", search_pattern, search_pattern)
+      @courses = Course.where("title ILIKE ? OR description ILIKE ?",
+                              "%#{search_pattern}%", "%#{search_pattern}%")
       if @courses.present?
         @categories = Category.where(id: @courses.pluck(:category_id).uniq).page(params[:page]).per(5)
+        @courses = Course.where(category_id: @categories.pluck(:id))
+                         .order(:title)
+                         .group_by(&:category_id)
+        render :index
       else
-        @categories = Category.joins(:courses).distinct.order(:name).page(params[:page]).per(5)
+        @categories = Category.select("DISTINCT categories.*")
+                              .joins(:courses)
+                              .order(:name)
+                              .page(params[:page])
+                              .per(ITEMS_PER_PAGE)
+        @courses = Course.where(category_id: @categories.pluck(:id))
+                         .order(:title)
+                         .group_by(&:category_id)
         flash[:notice] = t("no_courses_found")
-        redirect_to root_path
+        render :index
       end
     else
-      @categories = Category.joins(:courses).distinct.order(:name).page(params[:page]).per(5)
+      @categories = Category.select("DISTINCT categories.*")
+                            .joins(:courses)
+                            .order(:name)
+                            .page(params[:page])
+                            .per(ITEMS_PER_PAGE)
+      @courses = Course.where(category_id: @categories.pluck(:id))
+                       .order(:title)
+                       .group_by(&:category_id)
     end
   end
 end
