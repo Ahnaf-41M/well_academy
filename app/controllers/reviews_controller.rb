1cfd504d69701a73475098dfdc975088464c2539
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2025-08-01 20:57:34 +0600

    chore: update review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,13 @@
 class ReviewsController < ApplicationController
+  REVIEWS_PER_PAGE = 10
+
   load_and_authorize_resource
-  before_action :set_user
+
   before_action :set_review, only: %i[show edit update destroy]
   before_action :set_course, only: %i[new create edit update destroy]
 
   def index
-    @reviews = Review.where(course_id: params[:course_id]).page(params[:page]).per(5)
+    @reviews = Review.where(course_id: params[:course_id]).page(params[:page]).per(REVIEWS_PER_PAGE)
     @course = Course.find(params[:course_id])
     @teacher = @course.teacher
   end
@@ -19,13 +21,12 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @course.reviews.build(review_params)
-    @review.student_id = @user.id
+    @review.student_id = current_user.id
 
     if @review.save
-      redirect_to course_path(@course), notice: t('reviews.create.success')
+      redirect_to course_path(@course), notice: t("reviews.create.success")
     else
-      # flash.now[:alert] = @review.errors.full_messages.join(", ")
-      flash.now[:alert] = t('reviews.create.failure')
+      flash.now[:alert] = t("reviews.create.failure", error: @review.errors.full_messages.join(", "))
       render :new, status: :unprocessable_entity
     end
   end
@@ -35,30 +36,25 @@ class ReviewsController < ApplicationController
 
   def update
     if @review.update(review_params)
-      redirect_to course_path(@review.course), notice: t('reviews.update.success')
+      redirect_to course_path(@review.course), notice: t("reviews.update.success")
     else
-      # flash.now[:alert] = @review.errors.full_messages.join(", ")
-      flash.now[:alert] = t('reviews.update.failure')
+      flash.now[:alert] = t("reviews.update.failure", error: @review.errors.full_messages.join(", "))
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @review = @course.reviews.find_by(id: params[:id], student_id: @user.id)
+    @review = @course.reviews.find_by(id: params[:id], student_id: current_user.id)
 
     if @review
       @review.destroy
-      redirect_to course_path(@review.course), notice: t('reviews.destroy.success')
+      redirect_to course_path(@review.course), notice: t("reviews.destroy.success")
     else
-      redirect_to course_path(@course), alert: t('reviews.destroy.failure')
+      redirect_to course_path(@course), alert: t("reviews.destroy.failure")
     end
   end
 
   private
-
-  def set_user
-    @user = current_user
-  end
 
   def set_review
     @review = Review.find(params[:id])
