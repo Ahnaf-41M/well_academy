b459ba7d4b25905c50589fcfb6eefb71f6ba71d5
Md. Ahnaf Muttaqi Chowdhury <mdahnaf7@gmail.com>
2025-09-19 21:54:18 +0600

    fix courses controller spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "user#{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
-    phone { "1234567890" }
+    phone { "01234567890" }
     bio { "A test bio" }
 
     # Define the roles
